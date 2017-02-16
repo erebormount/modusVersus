5cab608a001885748958e793ed1a62ea375171cb
erebormount <davidpuinean@gmail.com>
2017-02-15 17:17:13 -0700

    Add files via upload

---
+++ js-file.js
@@ -0,0 +1,43 @@
+$('document').ready(function(){
+
+    $("#slider-one").roundSlider({
+	    radius: 60,
+	    circleShape: "half-top",
+	    sliderType: "min-range",
+	    showTooltip: true,
+	    value: 60,
+	    readOnly: true,
+	    handleSize: 0
+	});
+
+	$("#slider-two").roundSlider({
+	    radius: 60,
+	    circleShape: "half-top",
+	    sliderType: "min-range",
+	    showTooltip: true,
+	    value: 70,
+	    readOnly: true,
+	    handleSize: 0
+	});
+
+	$("#slider-three").roundSlider({
+	    radius: 60,
+	    circleShape: "half-top",
+	    sliderType: "min-range",
+	    showTooltip: true,
+	    value: 75,
+	    readOnly: true,
+	    handleSize: 0
+	});
+
+	$("#slider-four").roundSlider({
+	    radius: 60,
+	    circleShape: "half-top",
+	    sliderType: "min-range",
+	    showTooltip: true,
+	    value: 100,
+	    readOnly: true,
+	    handleSize: 0
+	});
+
+});
